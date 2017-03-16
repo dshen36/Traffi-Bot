ae0853bf4205e967a4ed7cf5e4a4e7deb24ce59c
dshen36 <dshen36@gatech.edu>
2017-03-15 23:56:50 -0400

    Added alerts to guide users when adding a robot

--- js/dashboard.js
+++ js/dashboard.js
@@ -16,6 +16,7 @@ function calculatePinColor(batteryLevel) {
 	}
 }
 
+// Logic used to edit text divs that look normal
 var defaultText = 'Edit Robot Name';
 
 function doneInput(i) {
@@ -41,4 +42,38 @@ $('.clickedit').hide()
 	.prev().click(function () {
     $(this).hide();
     $(this).next().show().focus();
+});
+
+
+
+
+// Unique ID coutner add-on
+var alert_counter = 0;
+
+// general method to create alerts
+bootstrap_alert = function () {}
+bootstrap_alert.warning = function (message, alert, timeout) {
+	var alert_id = "floating_alert_" + alert_counter++;
+    $('<div id=' + alert_id + ' class="alert alert-' + alert + ' alert-dismissable fade in"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>' + message + '</div>').appendTo('#content');
+
+    // closes the alert automatically after a timeout
+    setTimeout(function () {
+        $("#" + alert_id).alert('close');
+    }, timeout);
+}
+
+// Alert for marker add on
+$(document).ready(function () {
+    $('#add-robot-marker').click(function () {
+    	bootstrap_alert.warning('Please move the red marker to the desired traffic point of interest!', 'info', 4000);
+    })
+});
+
+// Alert for traffic boundaries for traffic intersection
+$(document).ready(function () {
+    $('#add-robot-boundaries').click(function () {
+    	bootstrap_alert.warning('Draw a small path through the previously marked point. \
+    		This will serve as the boundaries of this intersection. Click on the map <strong> near the road </strong>\
+    		and draw throught the intersection. Double click to stop drawing', 'info', 5000);
+    })
 });
