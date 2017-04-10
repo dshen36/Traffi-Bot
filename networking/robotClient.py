f4f3899cf35f7e1f1456a900df5a76c159a86167
stevenkcho <steven.k.cho@gmail.com>
2017-04-10 17:39:56 -0400

    Steven's code

--- networking/robotClient.py
+++ networking/robotClient.py
@@ -1,4 +1,4 @@
-import socket, sys, ctypes, string, serial
+import socket, sys, ctypes, string, time, threading
 
 # arguments = input('Enter port number and text-file name: \n')
 
@@ -12,49 +12,46 @@ port = int(sys.argv[1])
 spam_score = 0.0
 spam_words = ""
 port2 = "com4"
-ser = serial.Serial(port2, 9600)
+# ser = serial.Serial(port2, 9600)
 command = ""
 
 #TCP socket
-sock =  socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #binding socket to appropriate port
 server = ('',port) #replace with actual port later
 sock.bind(server)
 print("binded socket")
 
-
 # listen for incoming connection, and puts into server mode
 sock.listen(1)
-while True:
-    # if not command:
-    #     command = raw_input()
-    #     connection.sendall(command.strip())
 
+while True:
     # waits for an incoming connection
-    connection, client_address = sock.accept()
     print("waiting...")
+    connection, client_address = sock.accept()
+    print("connected")
 
     try:
-        if not command:
-            command = raw_input()
-            # connection.sendall(command.strip())
-        print("found connection")
         while True:
-            data = connection.recv(1024)
-            if data:
+            data = connection.recv(128)
+            if data == 'setup':
+                print('setup done')
+                connection.send('ok')
+            elif data == 'break':
+                print('cycle finished')
+                connection.send('ok')
+            elif data == 'end':
+                print('cycles done')
+                connection.send('ok')
+            elif data:
                 x = ser.write(data)
-                # connection.sendall('HI DAN')
+                # print(data)
+                connection.send('ok')
             #no more data, then exits
             else:
                 print("no more data")
                 break
-
     #closes the connection
     finally:
-        print("sent data")
-        #sys.exit(0)
-        #connection.close()
-        #ser.close()
-
-print("done")
+        print("-----Finished-----")
