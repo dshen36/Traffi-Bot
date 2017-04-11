bb0cc8e5328a7b452be408977e2968d82117eb3a
dshen36 <dshen36@gatech.edu>
2017-04-11 01:15:21 -0400

    Added content to faq page

---
+++ js/faq.js
@@ -0,0 +1,15 @@
+function DanBackground() {
+	$("#background-info").html("<p>Dan is a fourth year computer science major who not only drives around campus, but is also a pedestrian during the day. Because of that, he has experienced both sides of this problem and wanted to use his coding skills to solve this problem. He not only developed the frontend, but helped heavily on the backend.");
+}
+
+function JohnBackground() {
+	$("#background-info").html("<p>John is a fourth year mechanical engineer who has an electrical engineer background. Because of that, he has been a valuable asset in creating the hardware and physical portion of our project.");
+}
+
+function StevenBackground() {
+	$("#background-info").html("<p>Steven is a third year computer science major who worked extensively on the backend to develop a secure and live connection between the traffic light itself to a live server, which is controlled by a web application");
+}
+
+function JiSuBackground() {
+	$("#background-info").html("<p>JiSu is a fourth year computer science major who not only helped work on the frontend, but also desgined the wireframes that ultimately led to our current web application and physical device.");
+}
