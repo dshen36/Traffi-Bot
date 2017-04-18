a809a5c5389ea62f0b591d959e42374562adc9b8
dshen36 <dshen36@gatech.edu>
2017-04-17 20:12:21 -0400

    Set up Robot Control Panel

---
+++ js/control_robot.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+	$('#connect-robot').click(function () {
+		IP_Address = document.getElementById("IP-Address").value;
+		// TODO: @STEVEN
+
+
+	})
+
+	$('#start-button').click(function () {
+		Vehicle_Duration = document.getElementById("vehicle-duration").value;
+		Pedestrian_Duration = document.getElementById("pedestrian-duration").value;
+		// TODO: @STEVEN
+
+
+	})
+
+	$('#stop-button').click(function () {
+		// TODO: @STEVEN
+
+		
+	})
+});
+
+
+
