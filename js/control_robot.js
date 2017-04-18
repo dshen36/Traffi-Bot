8746b4e1430e8f6bac3aa8cd709ca08f2e4d1f18
stevenkcho <steven.k.cho@gmail.com>
2017-04-17 21:23:19 -0400

    Test alert

--- js/control_robot.js
+++ js/control_robot.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 	$('#connect-robot').click(function () {
 		IP_Address = document.getElementById("IP-Address").value;
 		// TODO: @STEVEN
-		alert(IP_Address);
+		alert("Hello");
 
 	})
 
@@ -10,13 +10,13 @@ $(document).ready(function () {
 		Vehicle_Duration = document.getElementById("vehicle-duration").value;
 		Pedestrian_Duration = document.getElementById("pedestrian-duration").value;
 		// TODO: @STEVEN
-
+		alert('V: ' + Vehicle_Duration + ', P: ' + Pedestrian_Duration);
 
 	})
 
 	$('#stop-button').click(function () {
 		// TODO: @STEVEN
-
+		alert('Stop');
 		
 	})
 });
